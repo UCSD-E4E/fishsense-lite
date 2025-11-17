96269283feb4db7a915bd08534f91bc6dddaff4c
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-17 00:22:18 -0800

    revert: use the datetime datatype again

--- src/fishsense_api_sdk/models/user.py
+++ src/fishsense_api_sdk/models/user.py
@@ -1,6 +1,8 @@
 """Model representing a user in the Fishsense API SDK."""
 
-from pydantic import AwareDatetime, BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel
 
 
 class User(BaseModel):
@@ -11,5 +13,5 @@ class User(BaseModel):
     email: str | None
     first_name: str | None
     last_name: str | None
-    last_activity: AwareDatetime | None
-    date_joined: AwareDatetime | None
+    last_activity: datetime | None
+    date_joined: datetime | None
