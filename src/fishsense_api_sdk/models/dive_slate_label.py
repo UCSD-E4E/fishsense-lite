2775249e89fe87c7579466736cb13416273ea452
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-23 19:56:06 -0800

    feat: add additional properties to dive slate labels

--- src/fishsense_api_sdk/models/dive_slate_label.py
+++ src/fishsense_api_sdk/models/dive_slate_label.py
@@ -1,7 +1,7 @@
 """Model for Slate Labels."""
 
 from datetime import datetime
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 
 from pydantic import BaseModel
 
@@ -14,6 +14,10 @@ class DiveSlateLabel(BaseModel):
     label_studio_task_id: int | None
     label_studio_project_id: int | None
     image_url: str | None
+    upside_down: bool | None
+    reference_points: List[Tuple[float, float]] | None
+    slate_rectangle: List[Tuple[float, float]] | None
+    skipped_points: List[int] | None
     updated_at: datetime | None
     completed: bool | None
     label_studio_json: Dict[str, Any] | None
