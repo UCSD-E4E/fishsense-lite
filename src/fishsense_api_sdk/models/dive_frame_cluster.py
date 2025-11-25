eccaff7e056ac0f571273884241089b98f6d8298
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-25 15:35:05 -0800

    fix: dive frame cluster id should be allowed to be none

--- src/fishsense_api_sdk/models/dive_frame_cluster.py
+++ src/fishsense_api_sdk/models/dive_frame_cluster.py
@@ -11,7 +11,7 @@ from fishsense_api_sdk.models.data_source import DataSource
 class DiveFrameCluster(BaseModel):
     """Model representing a cluster of frames within a dive."""
 
-    id: int
+    id: int | None
     image_ids: List[int]
     data_source: DataSource
     updated_at: datetime | None
