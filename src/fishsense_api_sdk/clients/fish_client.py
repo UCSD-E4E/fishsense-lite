61b66cd882277fe9a3d47e76cfe5c98a97fb7008
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-11-25 19:02:18 -0800

    feat: add get species by scientific name

--- src/fishsense_api_sdk/clients/fish_client.py
+++ src/fishsense_api_sdk/clients/fish_client.py
@@ -3,6 +3,26 @@ from fishsense_api_sdk.models.species import Species
 
 
 class FishClient(ClientBase):
+    async def get_species_by_scientific_name(
+        self, scientific_name: str
+    ) -> Species | None:
+        """Retrieve species information by scientific name.
+
+        Args:get_species_by_scientific_name
+            scientific_name (str): The scientific name of the species to retrieve.
+
+        Returns:
+            Species | None: The species object if found, otherwise None.
+        """
+        response = await self._get(f"/api/v1/fish/species/{scientific_name}")
+        response.raise_for_status()
+
+        json = response.json()
+        if json is None:
+            return None
+
+        return Species.model_validate(json)
+
     async def post(self, species: Species) -> int:
         """Create a new species entry in the Fishsense API.
 
